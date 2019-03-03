4901084ddec04194f40fd7cf4ab32ba37371823b
blackplusy <happyming07@sina.com>
2019-03-03 16:13:11 +0800

    Add files via upload

---
+++ 例子-0303-07.单分支.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+name=input('请输入吃热狗的人的名字:')
+
+if name=='王思聪':
+	print('真香！！！')
---
+++ 例子-0303-09.多分支.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+name=input('请输入您喜欢的主播：')
+if name=='大司马':
+	print('三角形达野')
+elif	name=='55开':
+	print('我没开挂！')
+elif	name=='张大仙':
+	print('不慌，吃口药')
+else:
+	print('来来来，关注走一播！刷个飞机上车！')
