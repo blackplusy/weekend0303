2eca9db8eef9d32447d6739600e64581f8872b64
blackplusy <happyming07@sina.com>
2019-03-03 11:23:04 +0800

    Add files via upload

---
+++ 例子-0303-01.python的输出.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+#设置字符集utf-8
+#中文字符集 GBK-2312
+
+#代表注释
+#print代表打印、输出到控制端
+#1.直接输出
+print('im heygor gaga')
+print(12345)
+
+#2.变量输出
+a=100
+print(a)
+b=20
+print(a+b)
+
+#3.函数输出
+#abs()    绝对值
+print(abs(-10))
+
+
+#4.格式化输出
+#%s	格式化显示字符串信息
+#%d	格式化显示数字信息
+
+print('************')
+a='python '
+b=1
+
+print('%s is %d'%(a,b))
+
+c='heygor'
+print('%s is handsome!!!' % c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 例子-0303-02.python的输入.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+#input() 括号中可以添加提示信息
+#name=input('请输入您的法号：')
+#print(type(name))
+#print('your name is %s'%name)
+#补充：type() 判断变量的数据类型
+
+#raw_input() 括号中添加提示信息
+s=raw_input('请输入您的大学：')
+print(s)
+
+
