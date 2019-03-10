96ab0257b7bf49721ab2a876b5eda85ea4d7ab6a
blackplusy <happyming07@sina.com>
2019-03-10 14:59:18 +0800

    Add files via upload

---
+++ 例子-0310-05.字典操作.py
@@ -0,0 +1,61 @@
+#coding=utf-8
+dic={'name':'8jie','age':18}
+dic1={'name':'tangsir'}
+
+print(dic)
+print(dic['name'])
+print(dic['age'])
+
+dic={'name':'8jie','age':18}
+print(dic)
+dic['name']='redhair'
+print(dic)
+dic['jingli']='kill'
+print(dic)
+print('*************88')
+dic={'name':'8jie','age':18}
+print(dic)
+del dic['name']
+print(dic)
+del dic
+#print(dic)
+
+
+dic={'name':'8jie','age':18}
+print(dic)
+dic.clear()
+print(dic)
+
+
+
+
+
+print('*************88')
+
+dic={'name':'heygor','QQ':110}
+print(dic.keys())
+for i in dic.keys():
+	print(i)
+
+
+
+print(dic.values())
+for i in dic.values():
+	print(i)
+
+print(dic.items())
+for i,j in dic.items():
+	print(i,j)
+
+
+
+
+
+
+
+
+
+
+
+
+
