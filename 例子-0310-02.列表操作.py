b4b2ecdf06ebc9c4773562318054729b5882a056
blackplusy <happyming07@sina.com>
2019-03-10 11:07:56 +0800

    Add files via upload

---
+++ 例子-0310-02.列表操作.py
@@ -0,0 +1,54 @@
+#coding=utf-8
+l=[1,2,3,4]
+l2=[1,'kobe',2,'rose']
+l3=[1,2,[3,4]]
+
+l=[1,2,3,4,5]
+print(l)
+
+for i in l:
+	print(i)
+
+if 1 in l:
+	print('1 is here!')
+l=['james','rose','kobe','no.3']
+print(l[0])
+print(l[-3])
+#print(l[6])
+print(l[1:])
+print(l[:-1])
+print(l[2:3])
+
+l1=[1,2,3]
+l2=['heygor','ladeng']
+print(l1+l2)
+print('*******')
+l=[1,2,3]
+print(l)
+l[-1]=0
+print(l)
+l[0]='gogo'
+print(l)
+
+
+
+l=['c','b','a']
+print(l)
+del l[-2]
+print(l)
+del l
+print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
